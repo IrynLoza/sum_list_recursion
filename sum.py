6670ffcae1917ab65fb789539fb70e8e79f3bebf
Iryna Loza <irynloza@gmail.com>
2020-10-28 10:47:33 -0700

    Complete sum_list

--- sum.py
+++ sum.py
@@ -24,6 +24,11 @@ The sum of an empty list is zero::
 def sum_list(nums):
     """Using recursion, return the sum of numbers in a list."""
 
+    if nums == []:
+        return 0
+
+    return nums[0] + sum_list(nums[1:])    
+
     
     
           
